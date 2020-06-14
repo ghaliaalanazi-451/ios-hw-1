7723ffe4d8edc71043f9cc41639f8c37cb5f667d
ghaliaalanazi-451 <66477134+ghaliaalanazi-451@users.noreply.github.com>
2020-06-14 07:07:24 +0300

    تم حل الواجب الاول

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -7,11 +7,13 @@
 
 let name = "Ahmad"
 
-name = "Salem"
+ let name1 = "Salem"
+
+// هنا غيرت المعلومات المكتوبة سابقا , لأنه لا يصح جمع أو القيام بأي عملية حسابية بين أنواع مختلفة من المعلومات
 
 
-var myWeight = 60
-var FriendWeight = 80.5
+var myWeight = 60.5
+var FriendWeight = 7033.5
 var height = 1.5
 
 
@@ -38,35 +40,41 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
+var firstName1 = "waad"
+var lastName1 = "alanezi"
+var email1 = "waad@hotmail.com"
+var phoneNumber1 = 01055112233
+var age1 = 16
+var country1 = "egypt"
+var area1 = "cairo"
+var password = 77663322
 var isKuwaiti = false
 
 //Person 2
 
-
-
-
-
-
-
+var firstName2 = "fawaz"
+var lastName2 = "almutairi"
+var email2 = "fawaz.al@gmail.com"
+var phoneNumber2 = 55332265
+var age2 = 25
+var country2 = "kuwait"
+var area2 = "andlus"
+var password2 = 99008877
+var isKuwaiti2 = true
 
 
 
 //Person 3
 
-
-
-
-
-
-
+var firstName3 = "rayan"
+var lastName3 = "alshamri"
+var email3 = "rayan78@outlook.com"
+var phoneNumber3 = 0547434323
+var age3 = 30
+var country3 = "KSA"
+var area3 = "Riyadh"
+var password3 = 9987645000
+var isKuwaiti3 = false
 
 
 /*
@@ -82,3 +90,52 @@ var isKuwaiti = false
 
  
  */
+// comparing
+
+
+firstName1 == firstName2
+firstName1 == firstName3
+firstName1 != firstName2
+firstName1 != firstName3
+firstName2 == firstName3
+firstName2 != firstName3
+
+
+age1 & age2 == age3
+age1 & age3 == age2
+age2 & age3 == age1
+
+
+age1 & age2 != age3
+age1 & age3 != age2
+age2 & age3 != age1
+
+
+
+area1 == area2
+area1 == area3
+area1 != area2
+area1 != area3
+area2 == area3
+area2 != area3
+
+
+age1 > 18
+age2 > 18
+age3 > 18
+
+
+age1 < 18
+age2 < 18
+age3 < 18
+
+age1 == 17
+age1 != 17
+
+age2 == 17
+age2 != 17
+
+age3 == 17
+age3 != 17
+
+              
